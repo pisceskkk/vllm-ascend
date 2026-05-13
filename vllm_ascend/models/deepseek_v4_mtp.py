52911ec9b4d472bc49392cf51abb12a3b93bf931
GDzhu01 <809721801@qq.com>
2026-05-13 19:56:01 +0800

    deepseekv4-mtp remove sharedfusedmoe
    Signed-off-by: GDzhu01 <809721801@qq.com>

--- vllm_ascend/models/deepseek_v4_mtp.py
+++ vllm_ascend/models/deepseek_v4_mtp.py
@@ -12,7 +12,7 @@ from vllm.config import VllmConfig
 from vllm.distributed import (get_tensor_model_parallel_rank,
                               get_tensor_model_parallel_world_size)
 from vllm.logger import init_logger
-from vllm.model_executor.layers.fused_moe import SharedFusedMoE
+from vllm.model_executor.layers.fused_moe import FusedMoE
 from vllm.model_executor.layers.layernorm import RMSNorm
 from vllm.model_executor.layers.logits_processor import LogitsProcessor
 from vllm.model_executor.layers.quantization import QuantizationConfig
@@ -272,7 +272,7 @@ class DeepSeekV4MTP(nn.Module, SupportsPP, DeepseekV2MixtureOfExperts):
             ("gate_up_proj", "up_proj", 1),
         ]
 
-        expert_params_mapping = SharedFusedMoE.make_expert_params_mapping(
+        expert_params_mapping = FusedMoE.make_expert_params_mapping(
             model=self.model,
             ckpt_gate_proj_name="gate_proj",
             ckpt_down_proj_name="down_proj",
